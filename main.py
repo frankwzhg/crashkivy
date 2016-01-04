e2acba34c9dfbaf10d0d621074d42c89accaab7b
frankwzhg <frankwzhg@hotmail.com>
2016-01-04 18:02:25 +0800

    First commit

---
+++ main.py
@@ -0,0 +1,67 @@
+from kivy.app import App
+import webbrowser
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+
+class KivyTutoRoot(BoxLayout):
+    def __init__(self,**kwargs):
+        super(KivyTutoRoot, self).__init__(**kwargs)
+
+        #List of previous screens
+        self.screen_list = []
+
+    def changeScreen(self, next_screen):
+        operations = "addition subtraction multiplication division".split()
+        question = None
+
+        if self.ids.kivy_screen_manger.current not in self.screen_list:
+            print self.screen_list
+            self.screen_list.append(self.ids.kivy_screen_manger.current)
+
+        if next_screen == "About this App":
+            self.ids.kivy_screen_manger.current = "about_screen"
+
+    def onBackBtn(self):
+        # check if there are any screen to go back
+        if self.screen_list:
+            # if there are screen we can go back to , the just do it
+            self.ids.kivy_screen_manager.current = self.screen_list.pop()
+            # saw we  don't want to close
+            return True
+        # no more screen to go back to
+        return False
+
+class MyfirstApp(App):
+    def __init__(self, **kwargs):
+        super(MyfirstApp, self).__init__(**kwargs)
+        Window.bind(on_keyboard=self.onBackBtn)
+
+    def onBackBtn(self, window, key, *args):
+        # user press back button
+        if key == 27:
+            return self.root.onBackBtn()
+
+    def build(self):
+        return KivyTutoRoot()
+
+    def getText(self):
+        return ("Hey there!\nThis App was built using"
+                "[b][ref=kivy]kivy[/ref][/b]\n"
+                "Feel free to look at the source code"
+                "[b][ref=source]here[/ref][/b].\n"
+                "This app is underthe [b][ref=mit]MIT License[/ref][/b]\n"
+                "My site: [b][ref=website]PyGopar.com[/ref][/b]")
+
+    def on_ref_press(self, instance, ref):
+        _dict = {
+            "source": "https://github.com/gopar/kivy-Tutor",
+            "website": "https://www.pygopar.com",
+            "kivy": "http://kivy.org/#home",
+            "mit": "https://github.com/gopar/kivy-Tutor/blod/master/LICENSE"
+        }
+        webbrowser.open(_dict[ref])
+
+
+
+if __name__ == "__main__":
+    MyfirstApp().run()
